13947a0a31d4ff143d99e687b320469e140db412
emin <e.ertac.p@gmail.com>
2025-09-26 12:21:25 +0100

    commit

--- src/LeetCode150/MergeSortedArray.ts
+++
@@ -1,2 +0,0 @@
-const nums1 = [1, 2, 3, 0, 0, 0];
-const nums2 = [2, 5, 6];
---
+++ src/LeetcodeBlind70/121.BestTimeToBuyStock.ts
@@ -0,0 +1,32 @@
+// psuedo
+
+// iterate only one time
+// store information that holds the lowest number
+// store information that creates best profit margin
+// return the best profit margin when loop is complated
+
+function maxProfit(prices: number[]): number {
+  let min = Infinity;
+  let bestProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    // update the min and max
+    min = Math.min(prices[i], min);
+    const profitOpportunity = prices[i] - min;
+    bestProfit = Math.max(profitOpportunity, bestProfit);
+  }
+  return bestProfit;
+}
+
+const array1 = [7, 1, 5, 3, 6, 4];
+const array2 = [7, 6, 4, 3, 1];
+
+const results = [array1, array2].map((eachArray) => {
+  return `The array is [${eachArray}]. For this array best profit result is ${maxProfit(
+    eachArray
+  )}`;
+});
+
+console.log(results);
+
+export {};
